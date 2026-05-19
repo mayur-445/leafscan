3964b51cf682f15932d03f844543e8da0d938199
Mayur <mayumayur445@gmail.com>
2026-05-19 12:15:03 +0530

    clean project

--- predict.py
+++ predict.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
-from tensorflow.keras.preprocessing import image
+from keras.preprocessing import image
 import numpy as np
 
 # Load model
 model = tf.keras.models.load_model("model.h5")
 
-# Image path (FIX THIS LINE)
-img_path = "test_image.jpg/test..jpg"
+# Image path (FIX THI
+# S LINE)
+img_path = "C:\Users\mayur m u\OneDrive\Desktop\dataset\test\Tomato___Early_blight\16ae1b64-2954-4135-bea5-d599ba56740c___RS_Erly.B 7649.JPG"
 
 # Load image
 img = image.load_img(img_path, target_size=(128,128))
