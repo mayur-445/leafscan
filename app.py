60e489c829b50da918f68331e1c0ab32cc04892d
Mayur <mayumayur445@gmail.com>
2026-05-23 11:22:52 +0530

    updated render port

--- app.py
+++ app.py
@@ -44,5 +44,5 @@ def index():
 
 # 🔥 IMPORTANT PART (FIXED)
 if __name__ == "__main__":
-    print("SERVER STARTING...")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
